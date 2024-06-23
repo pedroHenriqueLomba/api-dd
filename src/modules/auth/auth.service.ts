c3654136b6bf46350431668dab7a659ea254151d
Pedro Lomba <p.henrique.lomba@gmail.com>
2024-06-23 13:19:20 -0300

    Removing console.log

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -13,7 +13,6 @@ export class AuthService {
 
   async login(loginDto: LoginDto) {
     const user = await this.userService.findToLogin(loginDto.email);
-    console.log(user.password);
     if (user && bcrypt.compare(loginDto.password, user.password)) {
       const payload = { email: user.email, id: user._id, name: user.name };
       return { access_token: this.jwtService.sign(payload) };
